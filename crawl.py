94cc9b967fb8753b7321b117875b96ea97fe0ad9
fhdufhdu <fhdufhdu@naver.com>
2020-07-25 14:12:00 +0900

    20/07/25 14:11 commit

--- crawl.py
+++ crawl.py
@@ -16,19 +16,38 @@ class CrawlingNaver:
         path = "./chromedriver84.exe"
         options = Options()
         options.headless = True
+        self.url = url
         self.driver = webdriver.Chrome(executable_path=path)
-        self.driver.get(url)
+        self.prd_name = 0
+        self.price = 0
 
     def start_crawl(self):
+        self.main_paging()
+
+    def main_paging(self):
+        cnt = "1"
+        while True:
+            self.driver.get(self.url+cnt)
+            cnt = int(cnt)+1
+            cnt = str(cnt)
+            self.main_clicking()
+
+    def main_clicking(self):
         try:
             data = WebDriverWait(self.driver, 10)
         finally:
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             data = self.driver.find_elements_by_class_name("basicList_link__1MaTN")
+            price = self.driver.find_elements_by_class_name("basicList_price__2r23_")
             for idx in range(0, len(data)):
+                self.prd_name = data[idx].text
+                self.price = price[idx].text
+                print(self.prd_name)
+                print(self.price)
                 data[idx].click()
-                self.next_crawl(positive=True)
+                self.review_crawling(positive=True)
 
-    def next_crawl(self, positive=True):
+    def review_crawling(self, positive=True):
         self.driver.switch_to.window(self.driver.window_handles[-1])
         url = self.driver.current_url
         self.driver.get(url+"#revw")
@@ -38,7 +57,6 @@ class CrawlingNaver:
             if url.find("smartstore") == -1:
                 self.driver.close()
                 self.driver.switch_to.window(self.driver.window_handles[0])
-                print("test")
                 return
             option = self.driver.find_elements_by_class_name("option_sort")
             review_cnt = self.driver.find_element_by_id("review_preview").find_element_by_class_name("num").text.replace(',', '')
@@ -59,6 +77,7 @@ class CrawlingNaver:
             total_idx = math.ceil(int(review_cnt)/20)
             select_btn_idx = 0
 
+            review_list = []
             '''리뷰 페이지 클릭하면서 긁어오기'''
             while True:
                 data = self.driver.find_element_by_css_selector("#area_review_list > div.detail_list_review._review_list").find_elements_by_tag_name("li")
@@ -79,13 +98,17 @@ class CrawlingNaver:
                         continue
 
                     css_name = '#'+key.get_attribute("id")+' > div > div.cell_text._cell_text > div.area_text > p > span.wrap_label > span'
-                    print(key.find_element_by_class_name("number_grade").text)  #평점
-                    print(key.find_elements_by_class_name("text_info")[1].text) #날짜
-                    print(key.find_elements_by_class_name("text_info")[2].text) #옵션
+                    temp_dic = dict()
+                    temp_dic['평점'] = key.find_element_by_class_name("number_grade").text
+                    temp_dic['날짜'] = key.find_elements_by_class_name("text_info")[1].text
+                    temp_dic['옵션'] = key.find_elements_by_class_name("text_info")[2].text
                     if self.check_elem_css(css_name=css_name):
-                        print(key.find_element_by_css_selector(css_name))    #한달사용기
-                    print(key.find_element_by_class_name("text").text)  #리뷰
-                    print(key.find_element_by_class_name("count").text) #좋아요
+                        temp_dic['리뷰형태'] = key.find_element_by_css_selector(css_name).text
+                    else:
+                        temp_dic['리뷰형태'] = '일반'
+                    temp_dic['리뷰'] = key.find_element_by_class_name("text").text
+                    temp_dic['좋아요'] = key.find_element_by_class_name("count").text
+                    review_list.append(temp_dic)
 
                 '''리뷰 페이지 수 카운트'''
                 if idx == total_idx:
@@ -94,6 +117,7 @@ class CrawlingNaver:
                 select_btn_idx += 1
                 idx += 1
                 time.sleep(1)
+            self.json_file_save(review_cnt, review_list)
             self.driver.close()
             self.driver.switch_to.window(self.driver.window_handles[0])
 
@@ -114,10 +138,22 @@ class CrawlingNaver:
     def shutdown(self):
         self.driver.quit()
 
-crawl = CrawlingNaver(url="https://search.shopping.naver.com/search/all?query=탁구채&cat_id=&frm=NVSHATC")
+    def json_file_save(self, review_cnt, review_list):
+        with open("./review_file.json", "r", encoding='utf-8') as file:
+            json_dict = json.load(file)
+        add_json = {
+            'product_name': self.prd_name,
+            'product_price': self.price,
+            'review_cnt': review_cnt,
+            'review_list': review_list
+        }
+        json_dict['list'].append(add_json)
+        with open("./review_file.json", "w", encoding='utf-8') as make_file:
+            json.dump(json_dict, make_file, indent="\t", ensure_ascii=False)
+
+crawl = CrawlingNaver(url="https://search.shopping.naver.com/search/all?query=탁구채&cat_id=&frm=NVSHATC&pagingIndex=")
 crawl.start_crawl()
 crawl.shutdown()
-
 
 
 '''
@@ -128,5 +164,5 @@ crawl.shutdown()
 5.데이터베이스 구현
 리뷰내용, 시간, 별점(1~5), 상품옵션, 따봉, 한달사용기유무
 https://smartstore.naver.com/dbtak/products/315627908#revw
-"https://search.shopping.naver.com/search/all?query=탁구채&cat_id=&frm=NVSHATC"
+"https://search.shopping.naver.com/search/all?query=탁구채&cat_id=&frm=NVSHATC&pagingIndex=2"
 '''
